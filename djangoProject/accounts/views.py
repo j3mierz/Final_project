f1104f86f511d5541367ef112ee5a9cb3839e8ee
Julian Mierzejewski <j3mierz@gmail.com>
2024-06-10 13:40:41 +0200

    loging and register works

--- djangoProject/accounts/views.py
+++ djangoProject/accounts/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import authenticate, login
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 from django.views import View
 
 
@@ -10,12 +11,26 @@ class LoginView(View):
         return render(request, "accounts/login_view.html")
 
     def post(self, request):
-        username = request.POST('username')
-        password = request.POST('password')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
         user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
+            return redirect('login_view')
+        return render(request, 'accounts/login_view.html', {"error": "Invalid username or password."})
+
 
 class RegisterView(View):
     def get(self, request):
         return render(request, "accounts/register_view.html")
+
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        possword2 = request.POST.get('password2')
+        if password == possword2 and password != "":
+            user = User(username=username)
+            user.set_password(password)
+            user.save()
+            return redirect('login_view')
+        return render(request, "accounts/register_view.html", {'error': "passwords do not match"})
