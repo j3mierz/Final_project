8b630a9323e95d8dc753a91d7d954f78df72cd2b
Julian Mierzejewski <j3mierz@gmail.com>
2024-06-10 13:32:52 +0200

    register view add

--- djangoProject/accounts/views.py
+++ djangoProject/accounts/views.py
@@ -1,10 +1,21 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render
 from django.views import View
+
+
 # Create your views here.
 
 class LoginView(View):
     def get(self, request):
         return render(request, "accounts/login_view.html")
+
     def post(self, request):
         username = request.POST('username')
         password = request.POST('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+
+class RegisterView(View):
+    def get(self, request):
+        return render(request, "accounts/register_view.html")
